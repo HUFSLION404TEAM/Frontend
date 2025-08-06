abe99e6cdf9ee403956d72ee5d1e7e8aa5f0d235
Seung876 <sm08073469@gmail.com>
2025-08-07 03:03:35 +0900

    fix : 경로 수정하기

---
+++ src/pages/owner/Review/indes.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ReviewOwner = () => {
+  return (
+    <div style={{ padding: '40px', fontSize: '24px', textAlign: 'center' }}>
+    소상공인-리뷰 페이지입니다!
+    </div>
+  );
+};
+
+export default ReviewOwner;
