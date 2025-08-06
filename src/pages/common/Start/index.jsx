601052757515bbaa98514eea4f8ce077d3577a3d
심재서 <jaeseo0944@hufs.ac.kr>
2025-08-06 23:00:30 +0900

    메인 테스트

--- src/pages/common/Start/index.jsx
+++ src/pages/common/Start/index.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
 const Start = () => {
   return (
-    <div style={{ padding: '40px', fontSize: '24px', textAlign: 'center' }}>
-      👋 시작 페이지입니다!
+    <div style={{ padding: "40px", fontSize: "24px", textAlign: "center" }}>
+      👋 시작 페이지입니다! 프론트 힘내요~
     </div>
   );
 };
